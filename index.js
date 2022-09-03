8fbbbfb87c33b6d3970be92deba804420e1a9a99
Zane Rau <zanemrau@gmail.com>
2022-09-02 23:51:27 -0700

    Adding music bot capabilities

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 // Require the necessary discord.js classes
-const { Client, Intents} = require('discord.js');
+const { Client, GuildMember, GatewayIntentBits} = require('discord.js');
+
+// Class handlers required for the discord music player 
+const { Player} = require("discord-player");
+
+
 // Import events
 const RoleManager = require("./src/role_assigner.js");
+
+// Import music player events
+const MusicPlayer = require("./src/music_player.js");
+
 // Importat local configuration parameters
 require('dotenv').config();
 
 // Create a new client instance
-client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
+// client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_VOICE_STATES] });
+client = new Client({ intents: [
+	GatewayIntentBits.Guilds,
+	GatewayIntentBits.GuildMessages,
+	GatewayIntentBits.GuildMessageReactions,
+	GatewayIntentBits.GuildVoiceStates,
+	GatewayIntentBits.MessageContent
+] });
+player = new Player(client);
 
 // client.on('messageReactionAdd', async message => {
 // 	RoleManager.roleAssigner(message)
@@ -16,10 +33,26 @@ client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAG
 client.once('ready', () => {
 
 	RoleManager.roleManagerInit();
+	MusicPlayer.musicPlayerInit(GuildMember); // init music player
 	console.log('Ready!');
 });
 
-client.on("messageCreate", msg => {
+
+// Simple logs
+client.on("error", console.error);
+client.on("warn", console.warn);
+
+
+// Check messages
+client.on("messageCreate", async msg => {
+
+	// Exit if the message doesn't come from a person within the guild
+	if (msg.author.bot || !msg.guild) return;
+    	if (!client.application?.owner) await client.application?.fetch();
+
+	// Check for music deployment
+	MusicPlayer.musicPlayerDeploy(msg);
+
 	let str = msg.content;
 	switch(str) {
 		case "!ping":
---
+++ src/music_player.js
@@ -0,0 +1,154 @@
+
+function roleAssigner(message, user, add_or_remove) {
+
+	return;
+}
+
+
+async function musicPlayerDeploy(message) {
+
+	if (message.content === "!deploy" && message.author.id === client.application?.owner?.id) {
+		await message.guild.commands.set([
+		{
+			name: "play",
+			description: "Plays a song from youtube",
+			options: [
+			{
+				name: "query",
+				type: 3,
+				description: "The song you want to play",
+				required: true
+			}
+			]
+		},
+		{
+			name: "skip",
+			description: "Skip to the current song"
+		},
+		{
+			name: "queue",
+			description: "See the queue"
+		},
+		{
+			name: "stop",
+			description: "Stop the player"
+		},
+		]);
+
+		await message.reply("Deployed!");
+	}
+	
+}
+
+function musicPlayerInit(GuildMember) {
+	// const player = new Player(client);
+	// Class handlers required for the discord music player 
+	const QueryType = require("discord-player");
+
+	player.on("error", (queue, error) => {
+	    console.log(`[${queue.guild.name}] Error emitted from the queue: ${error.message}`);
+	});
+	player.on("connectionError", (queue, error) => {
+	    console.log(`[${queue.guild.name}] Error emitted from the connection: ${error.message}`);
+	});
+	player.on("trackStart", (queue, track) => {
+	    queue.metadata.send(`🎶 | Started playing: **${track.title}** in **${queue.connection.channel.name}**!`);
+	});
+	
+	player.on("trackAdd", (queue, track) => {
+	    queue.metadata.send(`🎶 | Track **${track.title}** queued!`);
+	});
+	
+	player.on("botDisconnect", (queue) => {
+	    queue.metadata.send("❌ | I was manually disconnected from the voice channel, clearing queue!");
+	});
+	
+	player.on("channelEmpty", (queue) => {
+	    queue.metadata.send("❌ | Nobody is in the voice channel, leaving...");
+	});
+	
+	player.on("queueEnd", (queue) => {
+	    queue.metadata.send("✅ | Queue finished!");
+	});
+
+
+	// Setup interaction between the slash commands
+	client.on("interactionCreate", async (interaction) => {
+		if(interaction.channelId !== '1005628193933365248') {
+			interaction.reply({
+			    content: "Unauthorized",
+			    ephemeral: true
+			});
+			return;
+		}
+		if (!interaction.isCommand() || !interaction.guildId) return;
+
+		if (!(interaction.member instanceof GuildMember) || !interaction.member.voice.channel) {
+		return void interaction.reply({ content: "You are not in a voice channel!", ephemeral: true });
+		}
+
+		// if (interaction.guild.me.voice.channelId && interaction.member.voice.channelId !== interaction.guild.me.voice.channelId) {
+		// if(interaction.member.voice.channelId){
+		// return void interaction.reply({ content: "You are not in my voice channel!", ephemeral: true });
+		// }
+
+		// Playing interaction
+		if (interaction.commandName === "play") {
+			await interaction.deferReply();
+
+			const query = interaction.options.get("query").value;
+			const searchResult = await player
+				.search(query, {
+				requestedBy: interaction.user,
+				searchEngine: QueryType.QueryType.AUTO
+				})
+				.catch(() => {});
+				if (!searchResult || !searchResult.tracks.length) return void interaction.followUp({ content: "No results were found!" });
+				const queue = await player.createQueue(interaction.guild, {
+					metadata: interaction.channel
+				});
+		
+				try {
+					if (!queue.playing) await queue.connect(interaction.member.voice.channel);
+				} catch {
+					void player.deleteQueue(interaction.guildId);
+					return void interaction.followUp({ content: "Could not join your voice channel!" });
+				}
+		
+				await interaction.followUp({ content: `⏱ | Loading your ${searchResult.playlist ? "playlist" : "track"}...` });
+				searchResult.playlist ? queue.addTracks(searchResult.tracks) : queue.addTrack(searchResult.tracks[0]);
+				if (!queue.playing) await queue.play();
+		}
+		// Skipping interaction
+		else if (interaction.commandName === "skip") {
+			await interaction.deferReply();
+			const queue = player.getQueue(interaction.guildId);
+			if (!queue || !queue.playing) return void interaction.followUp({ content: "❌ | No music is being played!" });
+			const currentTrack = queue.current;
+			const success = queue.skip();
+			return void interaction.followUp({
+				content: success ? `✅ | Skipped **${currentTrack}**!` : "❌ | Something went wrong!"
+			});
+		} else if (interaction.commandName === "stop") { 	// Stopping interaction 
+			await interaction.deferReply();
+			const queue = player.getQueue(interaction.guildId);
+			if (!queue || !queue.playing) return void interaction.followUp({ content: "❌ | No music is being played!" });
+			queue.destroy();
+			return void interaction.followUp({ content: "🛑 | Stopped the player!" });
+		}  else {
+			interaction.reply({
+			    content: "Unknown command!",
+			    ephemeral: true
+			});
+		}
+
+
+
+	});
+
+}
+
+module.exports = {
+	musicPlayerInit,
+	musicPlayerDeploy
+}
